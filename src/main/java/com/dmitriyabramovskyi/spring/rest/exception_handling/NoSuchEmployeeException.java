df6c4ce417e632faf01b9a26c55c14bc624f4ffc
DA_promos <d.abramovskyi@gmail.com>
2021-10-05 21:39:55 +0200

    Processing exception

---
+++ src/main/java/com/dmitriyabramovskyi/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,10 @@
+package com.dmitriyabramovskyi.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+
+
+}
