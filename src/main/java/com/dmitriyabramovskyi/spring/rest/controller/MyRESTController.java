7a2f6429c6a1c00a289af2425b42c610dca2a113
DA_promos <d.abramovskyi@gmail.com>
2021-10-06 22:14:03 +0200

    Global exception controller

--- src/main/java/com/dmitriyabramovskyi/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/dmitriyabramovskyi/spring/rest/controller/MyRESTController.java
@@ -30,29 +30,13 @@ public class MyRESTController {
 
         if(employee==null){
             throw new NoSuchEmployeeException("There is no employee with ID = " +
-                    + id + "int Database");
+                    + id + " int Database");
         }
 
         return employee;
     }
 
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(
-            NoSuchEmployeeException exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
 
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(
-            Exception exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
 
 
 }
